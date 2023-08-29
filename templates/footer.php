145b74382f72698dfc7d7f399ebd44ee25e9ccae
Castelli87 <davidecastelli87@hotmail.com>
2023-08-29 15:53:57 +0100

    Display list of recipes

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <footer class='bg-primary p-3 text-center'>
     <div>
-        <p>Copyright 2023 by DCsolutionsss</p>
+        <p class='text-white'>Copyright 2023 by DCsolutionsss</p>
     </div>
 
 </footer>
